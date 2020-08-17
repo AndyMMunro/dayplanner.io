4c99973f46485a295afcc4691b8efefa58f97791
AndyMMunro <agovitadventures@gmail.com>
2020-08-17 16:58:55 -0700

    changed format to be more close to gif provided added storage

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 //     - incopreate an date and time api
 
 // 2  then create entery blocks with save buttons
-//     - needds to be based on 24hrs
+//     - needs to be based on 24hrs
 //         - build calandar inside of HTML
 //             - delet event button added when input is placed into field
 
@@ -14,23 +14,95 @@
 
 // 5  
 
-// ?current Date and time diplay
-window.onload = function () {
+// ?current Date and time display
+$(document).ready(function () {
 
-    var currentDay = document.getElementById("currentDay");
+    // var currentDay = document.getElementById("currentDay");
+
+    var currentDay = $("#current-day");
 
     var dateTime = moment().format('MMMM Do YYYY, h:mm:ss a');
 
-    currentDay.innerHTML = dateTime;
+    $(currentDay).text(dateTime)
 
-    console.log(currentDay);
+    var rowArr = $(".time-block");
+    // console.log(rowArr);
 
-    var timeFieldDisplay = document.getElementsByClassName("time-field");
 
-    var time = moment().format("H")
+    var currentHour = moment().format('HH');
 
-    timeFieldDisplay.innerHTML = time;
+    rowArr.each(function () {
+        // console.log(this.innerText)
+        console.log($(this).data("hour"))
+        var thisHour = parseInt($(this).data("hour"));
 
-    console.log(timeFieldDisplay);
-}
+        if (thisHour == currentHour) {
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+            $(this).addClass("present")
+
+            console.log("this row is present " + $(this).data("hour"))
+        } else if (thisHour < currentHour) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past")
+            console.log("this row is past " + $(this).data("hour"))
+        } else if (thisHour > currentHour) {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future")
+            console.log("this row is future " + $(this).data("hour"))
+        }
+
+    });
+
+    $(".saveBtn").on("click", function () {
+        // get nearby values
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        // save in localStorage
+        localStorage.setItem(time, value);
+    });
+
+
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+
+
+    // for (var time = 6; time <= 18; time++) {
+
+    //     let index = time - 6;
+
+    //     let timeField = document.getElementsByClassName("time-field")
+
+    //     let timeDisplay = time;
+
+    //     timeField[index].innerHTML = timeDisplay;
+
+    //     console.log(timeField);
+
+    // }
+
+
+
+
+
+    // Time col 
+    // 1 place time or moment into time col
+    // 2 compare time element with the current time
+
+
+
+});
+
+
 
